fbe184df36b365bd3249dce431cba9b2841fd52a
Jason Mueller <jasonmueller17@yahoo.ca>
2024-05-02 22:05:49 -0400

    quick comments

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,6 @@ use App\Http\Controllers\MoviesController;
 // });
 
 Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
+// The controller is the class that will handle what happens when you send a get request.
+// the '/' is path that the route responds to. The controller is a class and the 'index' is a method in the class. the movies.index is the name of the route. Inside the controller we view the index page or any corresponding page.
 Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show');
